04befd78299d46ecb1c0358aec8deb01850dc72d
fnqdcker <44675056+fnqdcker@users.noreply.github.com>
2018-11-03 13:11:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
-#sed -i  's/\("id": "\).*/\1'"$UUID"'",/g'  /etc/v2ray/config.json
 echo -e "$UUID" 
+cat /etc/v2ray/config.json
+sed -i  's/\("id": "\).*/\1'"$UUID"'",/g'  /etc/v2ray/config.json
 cat /etc/v2ray/config.json
 
 #v2ray -config=/etc/v2ray/config.json
